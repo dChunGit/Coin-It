ce916b41b5d2cf151eee224e8690bf867ec21e64
David Chun <dchun@utexas.edu>
2018-05-24 13:03:08 -0500

    Update with final code

--- Code/Coin.h
+++
@@ -1,4 +0,0 @@
-/*
-* Function initializes the coin selector and sends a function to be called on an edge triggered interrupt
-*/
-void setupCoinSelector(void(*task)(void));
--- Code/Dac.h
+++
@@ -1,2 +0,0 @@
-void DAC_Init(void);
-void DAC_Out(uint16_t data);
